ae9be56b474085201ba497331bdefb1f0c72c1b9
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2023-02-11 18:00:53 +0900

    Add file upload without db.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+mod upload;
+
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body(include_str!("index.html"))
+}
 
 #[get("/health")]
 async fn health() -> impl Responder {
@@ -7,8 +14,14 @@ async fn health() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::scope("v1").service(health)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().service(home).service(
+            web::scope("v1")
+                .service(health)
+                .service(upload::upload_file),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
